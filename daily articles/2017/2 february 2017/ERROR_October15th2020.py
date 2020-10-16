e793f4974f691e2383d3aedc019e952326ab115d
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-15 19:56:56 -0700

    ERROR_October15th2020.py
    An error from October 15th 2020.

---
+++ daily articles/2017/2 february 2017/ERROR_October15th2020.py
@@ -0,0 +1,6 @@
+# Error from October 15th 2020
+
+print("I made an error when importing this on October 15th 2020: I named the February 2nd directory with the name of the month directory. I can't delete or re-upload at the moment.")
+print("The original directory name was:\n/2 february 2nd 2017/");
+
+# End
